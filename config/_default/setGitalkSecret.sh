d2cc286fb150c38124de7e9e9d443b11f291b99a
canliang.dcl <canliang.dcl@alibaba-inc.com>
2021-04-07 13:57:42 +0800

    信息打印

--- config/_default/setGitalkSecret.sh
+++ config/_default/setGitalkSecret.sh
@@ -2,6 +2,9 @@
 
 # CURRENT_FOLDER=$(cd "$(dirname "$0")";pwd)
 
+echo "CLIENT_ID: $CLIENT_ID"
+echo "CLIENT_SECRET: $CLIENT_SECRET"
+
 sed -i '/\# gitalk/ r gitalk.template' config.toml
 
 sed -i "s/\${CLIENT_ID}/$CLIENT_ID/g" config.toml
